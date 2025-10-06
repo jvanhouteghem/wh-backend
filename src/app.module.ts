fa8993d6ba6718fc9bb650170c9c130ba1d2945e
VanhouteghemJ <vanhouteghem.j@gmail.com>
2025-10-07 01:35:01 +0200

    feat(security): add ThrottlerException

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { SurveyModule } from './survey/survey.module';
 import {RequestIdMiddleware} from "./middlewares/request-logger.middleware";
+import {ThrottlerGuard, ThrottlerModule} from "@nestjs/throttler";
+import {APP_GUARD} from "@nestjs/core";
 
 @Module({
-    imports: [PrismaModule, SurveyModule],
+    imports: [
+        PrismaModule,
+        ThrottlerModule.forRoot({
+            throttlers: [
+                {
+                    name: 'default',
+                    ttl: 60000,
+                    limit: 1,
+                },
+            ],
+        }),
+        SurveyModule
+    ],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+    ],
 })
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
